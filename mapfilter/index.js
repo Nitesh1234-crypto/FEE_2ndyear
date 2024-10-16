1baf6347c0fcb549047cf83e84000487b2e6b71c
Nitesh <nitesh.e21@nsut.ac.in>
2024-10-16 12:40:59 +0530

    resolve doubt in differenc between map and filter

---
+++ mapfilter/index.js
@@ -0,0 +1,20 @@
+let arr= [1,2,3,4];
+// let newarr=arr.map((e)=>{
+//   return e*2
+// })
+// console.log(newarr)
+
+// let oddarr=arr.filter((e)=>{
+//   if(e%2!=0) return e;
+// })
+// console.log(oddarr)
+// let oddarr2=arr.map((e)=>{
+//     if(e%2!=0) return e;
+//   })
+
+// console.log(oddarr2)
+
+let oddarr3=arr.filter((e)=>{
+    if(e%2!=0) return "kon banega krore pati";
+  })
+  console.log(oddarr3)
